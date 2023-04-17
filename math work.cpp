1c2b80713b46c15cb460f1571fd42c2a3858c9b8
Jesse7-Anderson <129820961+Jesse7-Anderson@users.noreply.github.com>
2023-04-17 17:10:32 +0300

    update 2
    more about the for loop

--- math work.cpp
+++ math work.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(){
-	printf(" have seen the code\n");
-	printf("yeah!");
+	float a;
+	a=3000.00;
+	for (a*=1.5;a<1500000;a+=a*0.5){
+		printf ("%.1f\n",a);
+	};
+	//try it out
 	
 }
